a627dec069fba4eaa91380bf9deb28cd57aecb20
aeoneons <Jessalynliang03@gmail.com>
2026-01-13 15:02:18 -0500

    Start

---
+++ main.py
@@ -0,0 +1,6 @@
+Choice = input('Do you want to start the game? (yes/no)')
+
+if Choice.lower() == 'yes':
+    print("Enemy started a fight with you!")
+else:
+    print("You chose not to start the game or misspelled yes.")
