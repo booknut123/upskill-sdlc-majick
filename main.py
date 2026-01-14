8a6241f139d5bc0ae98d68e603eb4e9ee99dc392
Amorie Zsako <az123@wellesley.edu>
2026-01-14 10:43:42 -0500

    added roll()

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import random
+
 """
 Docstring for main
 
 methods: attack(attacker, attackee), subtracts dmg val from attackee's HP, checks their HP val
+roll(), returns a random number between 1-20
 """
 
 choice = input('Do you want to start the game? (yes/no)')
@@ -10,3 +13,7 @@ if choice.lower() == 'yes':
     print("Enemy started a fight with you!")
 else:
     print("You chose not to start the game or misspelled yes.")
+    
+
+def roll():
+    return random.randint(1,20)
