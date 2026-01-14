df1bc661ea3380c2f00344702ca861cbc91e1402
booknut123 <kailynmlau@gmail.com>
2026-01-14 10:43:00 -0500

    added game start and init

--- main.py
+++ main.py
@@ -4,9 +4,40 @@ Docstring for main
 methods: attack(attacker, attackee), subtracts dmg val from attackee's HP, checks their HP val
 """
 
+def game():
+    print("Welcome to Majick!")
+    print("Battle starting.")
+    
+    battle()
+
+def battle():
+    print("An enemy approaches you. ")
+    _ = input("Roll for initiative! (enter any character) ")
+    player_roll = roll()
+    enemy_roll = roll()
+    while(player_roll == enemy_roll):
+        _ = input("You tied. Roll again! (enter any character) ")
+        player_roll = roll()
+        enemy_roll = roll()
+    if (player_roll > enemy_roll):
+        print("You rolled higher!")
+        player_turn()
+    else:
+        print("The enemy rolled higher. Watch out!")
+        enemy_turn()
+        
+
+
+
+
+
+
+
+
+
 choice = input('Do you want to start the game? (yes/no)')
 
 if choice.lower() == 'yes':
-    print("Enemy started a fight with you!")
+    game()
 else:
     print("You chose not to start the game or misspelled yes.")
